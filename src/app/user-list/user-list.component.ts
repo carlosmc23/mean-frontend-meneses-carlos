ada29fdd1e6b0dd6d6e2c5a8aa8a296c65533dd4
Carlos Meneses <kenshinmc23@gmail.com>
2018-03-06 20:48:44 -0400

    data binding interpolacion n items 6.-

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -8,14 +8,57 @@ import { User } from '../model/user';
 })
 export class UserListComponent implements OnInit {
 
-  usertest: User = {
+  userList: User[] = [];
+  /*usertest: User = {
     name: 'Carlos',
     lastname: 'Beltran',
     username: 'carlos123',
     email: 'carlos@correo.com',
     avatar: 'avatarCarlos'
-  };
-  constructor() { }
+  };*/
+
+  constructor() {
+    this.userList.push({
+      name: 'Carlos 0',
+      lastname: 'Beltran',
+      username: 'carlos123',
+      email: 'carlos@correo.com',
+      avatar: 'avatarCarlos'
+    });
+    this.userList.push({
+      name: 'Carlos 1',
+      lastname: 'Beltran',
+      username: 'carlos123',
+      email: 'carlos@correo.com',
+      avatar: 'avatarCarlos'
+    });
+    this.userList.push({
+      name: 'Carlos 2',
+      lastname: 'Beltran',
+      username: 'carlos123',
+      email: 'carlos@correo.com',
+      avatar: 'avatarCarlos'
+    });
+    this.userList.push({
+      name: 'Carlos 3',
+      lastname: 'Beltran',
+      username: 'carlos123',
+      email: 'carlos@correo.com',
+      avatar: 'avatarCarlos'
+  });
+  this.userList.push({
+    name: 'Carlos 4',
+    lastname: 'Beltran',
+    username: 'carlos123',
+    email: 'carlos@correo.com',
+    avatar: 'avatarCarlos'
+});
+
+
+
+
+
+  }
 
   ngOnInit() {
   }
